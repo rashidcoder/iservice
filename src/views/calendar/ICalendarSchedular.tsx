3f3f1e6dedfc851f0b8adaef564b8a274c53c18d
danish12github <danishalikhan688@gmail.com>
2019-11-06 15:49:11 +0500

    calander is customize
    Adding the ICalander.tsx file . for tsx file installed the dependencies like @type/react , @type-react-props through npm
    Calander is now dynamic

---
+++ src/views/calendar/ICalendarSchedular.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import {
+  Inject,
+  ScheduleComponent,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  EventSettingsModel
+} from '@syncfusion/ej2-react-schedule';
+import { DataManager,WebApiAdaptor } from "@syncfusion/ej2-data";
+class ICalenderSchedular extends React.Component {
+   private localData:EventSettingsModel ={
+    dataSource:[{
+        EndTime:new Date(2019,0,11,6,30),
+        StartTime:new Date(2019,0,11,4,0)
+    }]
+      };
+      private remoteData=new DataManager({
+        url:'https://js.syncfusion.com/demos/ejservices/api/Schedule/LoadData',
+        adaptor: new WebApiAdaptor,
+        crossDomain:true
+      });
+   public render() {
+    return (
+      <ScheduleComponent currentView='Month' selectedDate={new Date(2017,5,5)} eventSettings={{dataSource:this.remoteData}}   >
+             
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+      </ScheduleComponent>
+    );
+  }
+}
+export default ICalenderSchedular;
+
+
+
+/*
+
+import React from 'react';
+import {
+  Inject,
+  ScheduleComponent,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  EventSettingsModel
+} from '@syncfusion/ej2-react-schedule';
+class ICalenderSchedular extends React.Component {
+private localData:EventSettingsModel ={
+    dataSource:[{
+        EndTime:new Date(2019,10,11,6,30),
+        StartTime:new Date(2019,10,11,4,30),
+    }]
+      };
+    
+    render() {
+    return (
+      <ScheduleComponent currentView='Month' 
+         eventSettings={this.localData}  >
+    
+    <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+    </ScheduleComponent>
+  );
+}
+}
+export default ICalenderSchedular;
+*/
