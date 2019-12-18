262bcceae06b47ca058edb34f679052e259aedb6
Rashid Iqbal <35493528+rashidcoder@users.noreply.github.com>
2019-12-18 18:22:42 +0500

    fixed index.s

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
-<< << << < HEAD
 // // import React from 'react'
 // // import ReactDOM from 'react-dom'
 // // import 'semantic-ui-css/semantic.min.css'
 // // import App from './views/app/App'
 // // import * as serviceWorker from './serviceWorker'
 // // ReactDOM.render( <App/> , document.getElementById('root'))
-// // serviceWorker.unregister()
-    ===
-    ===
-    =
-    import React from 'react'
+
+import React from 'react'
 import ReactDOM from 'react-dom'
 import 'semantic-ui-css/semantic.min.css'
 import App from './views/app/App'
 import * as serviceWorker from './serviceWorker'
 ReactDOM.render( < App / > , document.getElementById('root'))
-serviceWorker.unregister() >>>
-    >>>
-    > 6965 b795a84f5220297f0543200251253bbb8938
+
 
 // var request = indexedDB.open('library')
 // var db
